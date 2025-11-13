a43ed56e91b29eb88524c1f3b3d067b33810b53f
snarky1980 <jskennedy80@gmail.com>
2025-11-13 13:11:51 +0000

    Updates

--- assets/admin-excel.js
+++
@@ -1,5 +0,0 @@
-// Deprecated Excel assistant script. Redirect to admin-simple and no-op.
-(function(){
-  try { console.warn('[admin-excel] This page is retired. Redirecting to admin-simple.html'); } catch {}
-  try { window.location.replace('../admin-simple.html'); } catch {}
-})();
